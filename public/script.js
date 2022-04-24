c649f2f1fa1c8c40ed3c6a63fa84ae4aab4d389b
Aliif Arief <aliif.arief.m@gmail.com>
2022-04-24 21:50:05 +0700

    fixing: session storage

--- public/script.js
+++ public/script.js
@@ -35,6 +35,8 @@ var doaClean = `<div><div>`
 
 function createViewSupag() {
 
+    sessionStorage.setItem('mode', 'sugro-pagi');
+
     let loader = `<div class="flex w-max justify-center items-center">
                   <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
@@ -87,11 +89,11 @@ function createViewSupag() {
     dynamicBrowser.innerText = "Al - Matsurat Sugro Pagi";
     dynamicTitle.innerText = "Al - Matsurat Sugro Pagi";
     dynamicDescription.innerText = "Biasakan membaca Al - Matsurat sugro setiap pagi";
-
-    sessionStorage.setItem('mode', 'sugro-pagi');
 }
 
 function createViewSupet() {
+
+    sessionStorage.setItem('mode', 'sugro-petang');
 
     let loader = `<div class="flex w-max justify-center items-center">
                   <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -145,8 +147,6 @@ function createViewSupet() {
     dynamicBrowser.innerText = "Al - Matsurat Sugro Petang";
     dynamicTitle.innerText = "Al - Matsurat Sugro Petang";
     dynamicDescription.innerText = "Biasakan membaca Al - Matsurat sugro setiap petang";
-
-    sessionStorage.setItem('mode', 'sugro-petang');
 }
 
 function createViewKupag() {

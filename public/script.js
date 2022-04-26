abc965d98cef499058ed2c082df7fd2cd394a0b6
Aliif Arief <aliif.arief.m@gmail.com>
2022-04-26 07:25:18 +0700

    add: dark mode

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ var dynamicView = document.getElementById('content');
 var backbutton = document.getElementById('back-button');
 var doaView = document.getElementById('doa');
 
-var clean = `<div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mx-4">
+var clean = `<div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mx-4 dark:text-white">
                 <a onclick="createViewSupag()">
                     <div class="flex cursor-pointer justify-center items-center h-28 border border-gray-400 hover:shadow-xl">
                         <p class="text-2xl">Sugro Pagi</p>
@@ -36,7 +36,7 @@ function createViewSupag() {
     var doaList = "";
 
     let loader = `<div class="flex w-[100%] justify-center items-center">
-                  <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-zinc-800 dark:fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
                   </svg>
@@ -58,8 +58,8 @@ function createViewSupag() {
                                     <p>${item.title}</p>
                                     <p>${item.repeat} repetisi</p>
                                 </div>
-                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6" dir="rtl">${item.core}</p> 
-                                <p class="text-justify mb-6">${item.terjemah}</p>
+                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6 dark:text-zinc-300" dir="rtl">${item.core}</p> 
+                                <p class="text-justify mb-6 dark:text-zinc-300">${item.terjemah}</p>
                             </div>`;
             }
             dynamicView.innerHTML = itemList
@@ -76,8 +76,8 @@ function createViewSupag() {
                                     <p>${doa.title}</p>
                                     <p>${doa.repeat} repetisi</p>
                                 </div>
-                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6" dir="rtl">${doa.core}</p> 
-                                <p class="text-justify mb-6">${doa.terjemah}</p>
+                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6 dark:text-zinc-300" dir="rtl">${doa.core}</p> 
+                                <p class="text-justify mb-6 dark:text-zinc-300">${doa.terjemah}</p>
                             </div>`;
             }
             doaView.innerHTML = doaList
@@ -97,7 +97,7 @@ function createViewSupet() {
     var doaList = "";
 
     let loader = `<div class="flex w-[100%] justify-center items-center">
-                  <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-zinc-800 dark:fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
                   </svg>
@@ -119,8 +119,8 @@ function createViewSupet() {
                                     <p>${item.title}</p>
                                     <p>${item.repeat} repetisi</p>
                                 </div>
-                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6" dir="rtl">${item.core}</p> 
-                                <p class="text-justify mb-6">${item.terjemah}</p>
+                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6 dark:text-zinc-300" dir="rtl">${item.core}</p> 
+                                <p class="text-justify mb-6 dark:text-zinc-300">${item.terjemah}</p>
                             </div>`;
             }
             dynamicView.innerHTML = itemList
@@ -137,8 +137,8 @@ function createViewSupet() {
                                     <p>${doa.title}</p>
                                     <p>${doa.repeat} repetisi</p>
                                 </div>
-                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6" dir="rtl">${doa.core}</p> 
-                                <p class="text-justify mb-6">${doa.terjemah}</p>
+                                <p class="text-3xl text-justify leading-relaxed font-['fontArab'] my-6 dark:text-zinc-300" dir="rtl">${doa.core}</p> 
+                                <p class="text-justify mb-6 dark:text-zinc-300">${doa.terjemah}</p>
                             </div>`;
             }
             doaView.innerHTML = doaList
@@ -183,5 +183,30 @@ function adaptiveView() {
         createViewSupet();
     }
 
+    if (nowview === null) {
+        resetViewBack();
+    }
+
     console.log(nowview)
+}
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
